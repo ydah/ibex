2e1bf3eaf3fa621d7dfe1ed3380834b6c9479b88
ydah <t.yudai92@gmail.com>
2026-07-22 18:02:54 +0900

    Add Ruby grammar DSL

---
+++ lib/ibex/frontend/dsl.rb
@@ -0,0 +1,143 @@
+# frozen_string_literal: true
+
+module Ibex
+  module Frontend
+    # Builds the same Grammar AST as the text frontend through a Ruby API.
+    module DSL
+      module_function
+
+      def grammar(class_name:, superclass: nil, file: "(dsl)")
+        builder = Builder.new(class_name: class_name, superclass: superclass, file: file)
+        yield builder
+        builder.to_ast
+      end
+
+      # Mutable definition context whose output is an immutable-shape AST.
+      class Builder
+        def initialize(class_name:, superclass:, file:)
+          @class_name = class_name.to_s
+          @superclass = superclass&.to_s
+          @file = file
+          @line = 1
+          @declarations = []
+          @rules = []
+          @user_code = Hash.new { |hash, key| hash[key] = [] }
+        end
+
+        def token(*names)
+          @declarations << AST::Tokens.new(names: names.map(&:to_s), loc: next_location)
+        end
+
+        def options(*names)
+          @declarations << AST::Options.new(names: names.map(&:to_s), loc: next_location)
+        end
+
+        def expect(conflicts)
+          @declarations << AST::Expect.new(conflicts: conflicts, loc: next_location)
+        end
+
+        def start(name)
+          @declarations << AST::Start.new(name: name.to_s, loc: next_location)
+        end
+
+        def convert(name, expression)
+          location = next_location
+          pair = AST::Conversion.new(name: name.to_s, expression: expression.to_s, loc: location)
+          @declarations << AST::Convert.new(pairs: [pair], loc: location)
+        end
+
+        def precedence(direction: :low_to_high)
+          location = next_location
+          builder = PrecedenceBuilder.new(self)
+          yield builder
+          @declarations << AST::Precedence.new(direction: direction, levels: builder.levels, loc: location)
+        end
+
+        def rule(lhs)
+          location = next_location
+          builder = RuleBuilder.new(self, location)
+          yield builder
+          @rules << AST::Rule.new(lhs: lhs.to_s, alternatives: builder.alternatives, loc: location)
+        end
+
+        def user_code(name, code)
+          key = name.to_s
+          @user_code[key] << AST::UserCode.new(name: key, code: code.to_s, loc: next_location)
+        end
+
+        def ref(name, as: nil)
+          AST::SymbolReference.new(name: name.to_s, named_reference: as&.to_s, loc: next_location)
+        end
+
+        def optional(item) = AST::Optional.new(item: normalize_item(item), loc: next_location)
+        def star(item) = AST::Star.new(item: normalize_item(item), loc: next_location)
+        def plus(item) = AST::Plus.new(item: normalize_item(item), loc: next_location)
+
+        def separated_list(item, separator, nonempty: false)
+          AST::SeparatedList.new(item: normalize_item(item), separator: normalize_item(separator),
+                                 nonempty: nonempty, loc: next_location)
+        end
+
+        def inline(code)
+          AST::InlineAction.new(code: code.to_s, loc: next_location)
+        end
+
+        def to_ast
+          location = Location.new(file: @file, line: 1, column: 1)
+          AST::Root.new(class_name: @class_name, superclass: @superclass, declarations: @declarations,
+                        rules: @rules, user_code: @user_code, loc: location)
+        end
+
+        def normalize_item(item)
+          return item if item.respond_to?(:loc)
+
+          ref(item)
+        end
+
+        def next_location
+          location = Location.new(file: @file, line: @line, column: 1)
+          @line += 1
+          location
+        end
+      end
+
+      # Collects ordered associativity levels.
+      class PrecedenceBuilder
+        attr_reader :levels
+
+        def initialize(grammar)
+          @grammar = grammar
+          @levels = []
+        end
+
+        %i[left right nonassoc].each do |associativity|
+          define_method(associativity) do |*symbols|
+            @levels << AST::PrecedenceLevel.new(associativity: associativity, symbols: symbols.map(&:to_s),
+                                                loc: @grammar.next_location)
+          end
+        end
+      end
+
+      # Collects alternatives for one nonterminal.
+      class RuleBuilder
+        attr_reader :alternatives
+
+        def initialize(grammar, default_location)
+          @grammar = grammar
+          @default_location = default_location
+          @alternatives = []
+        end
+
+        def alt(*items, action: nil, precedence: nil)
+          location = @grammar.next_location || @default_location
+          normalized = items.map { |item| @grammar.normalize_item(item) }
+          action_node = action && AST::InlineAction.new(code: action.to_s, loc: location)
+          alternative = AST::Alternative.new(
+            items: normalized, action: action_node, precedence: precedence&.to_s, loc: location
+          )
+          @alternatives << alternative
+        end
+      end
+    end
+  end
+end
---
+++ test/frontend/dsl_test.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+
+class FrontendDSLTest < Minitest::Test
+  def test_text_and_dsl_frontends_produce_semantically_equal_ir
+    text = <<~GRAMMAR
+      class DSLCalc
+      token NUM
+      preclow
+      left '+'
+      prechigh
+      start expr
+      rule
+      expr: expr '+' expr { result = val[0] + val[2] }
+          | NUM { result = val[0] }
+      end
+      ---- inner
+      def marker = :text
+    GRAMMAR
+    text_ast = Ibex::Frontend::Parser.new(text, file: "text.y").parse
+    dsl_ast = Ibex::Frontend::DSL.grammar(class_name: "DSLCalc") do |grammar|
+      grammar.token(:NUM)
+      grammar.precedence { |levels| levels.left("'+'") }
+      grammar.start(:expr)
+      grammar.rule(:expr) do |rule|
+        rule.alt(:expr, "'+'", :expr, action: " result = val[0] + val[2] ")
+        rule.alt(:NUM, action: " result = val[0] ")
+      end
+      grammar.user_code(:inner, "def marker = :text\n")
+    end
+    text_ir = Ibex::Normalizer.new(text_ast).normalize.to_h
+    dsl_ir = Ibex::Normalizer.new(dsl_ast).normalize.to_h
+    assert_equal without_locations(text_ir), without_locations(dsl_ir)
+  end
+
+  def test_dsl_supports_extended_items_and_named_references
+    ast = Ibex::Frontend::DSL.grammar(class_name: "DSLList") do |grammar|
+      grammar.rule(:start) do |rule|
+        items = grammar.star(grammar.ref(:ITEM, as: :items))
+        rule.alt(items, action: " result = items ")
+      end
+    end
+    grammar = Ibex::Normalizer.new(ast, mode: :extended).normalize
+    user = grammar.productions.last
+    assert_equal [{ name: "items", index: 0 }], user.action.named_refs
+    assert(grammar.productions.any? { |production| production.origin[:kind] == :star_expansion })
+  end
+
+  def test_dsl_supports_convert_options_and_user_code
+    ast = Ibex::Frontend::DSL.grammar(class_name: "Configured") do |grammar|
+      grammar.token(:NUM)
+      grammar.options(:no_result_var)
+      grammar.expect(2)
+      grammar.convert(:NUM, ":number")
+      grammar.rule(:start) { |rule| rule.alt(:NUM, action: " val[0] ") }
+      grammar.user_code(:header, "HEADER\n")
+    end
+    ir = Ibex::Normalizer.new(ast).normalize
+    refute ir.options[:result_var]
+    assert_equal 2, ir.expect
+    assert_equal ":number", ir.conversions["NUM"]
+    assert_equal "HEADER\n", ir.user_code["header"]
+  end
+
+  private
+
+  def without_locations(value)
+    case value
+    when Array then value.map { |item| without_locations(item) }
+    when Hash
+      value.each_with_object({}) do |(key, item), result|
+        result[key] = without_locations(item) unless key.to_sym == :loc
+      end
+    else value
+    end
+  end
+end
