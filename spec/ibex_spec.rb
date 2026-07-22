1bce63f7734a6df2bef3f9d7de87680093a8d2a2
ydah <t.yudai92@gmail.com>
2026-07-22 17:23:05 +0900

    Initial commit

---
+++ spec/ibex_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ibex do
+  it "has a version number" do
+    expect(Ibex::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
