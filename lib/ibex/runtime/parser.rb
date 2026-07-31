f2a1421c8f02ffb4d50726452be6eae73cf7f510
ydah <t.yudai92@gmail.com>
2026-07-31 18:13:53 +0900

    Keep dormant CST state signature-neutral

--- lib/ibex/runtime/parser.rb
+++ lib/ibex/runtime/parser.rb
@@ -254,19 +254,14 @@ module Ibex
       empty_location_names = {} # @type var empty_location_names: Hash[Symbol, Integer]
       empty_locations = [] # @type var empty_locations: Array[untyped]
       empty_green_trivia = [] # @type var empty_green_trivia: Array[CST::GreenTrivia]
-      empty_green_token_states = [] # @type var empty_green_token_states: Array[Symbol]
-      empty_green_memo_entries = [] # @type var empty_green_memo_entries: Array[CST::ParseMemo::Entry]
 
       EMPTY_ROW = empty_row.freeze #: Hash[Integer, untyped]
       EMPTY_LOCATION_NAMES = empty_location_names.freeze #: Hash[Symbol, Integer]
       EMPTY_LOCATIONS = empty_locations.freeze #: Array[untyped]
       EMPTY_GREEN_TRIVIA = empty_green_trivia.freeze #: Array[CST::GreenTrivia]
-      EMPTY_GREEN_TOKEN_STATES = empty_green_token_states.freeze #: Array[Symbol]
-      EMPTY_GREEN_MEMO_ENTRIES = empty_green_memo_entries.freeze #: Array[CST::ParseMemo::Entry]
       private_constant :ERROR_ACTION, :SYNC_RECOVER_ACTION, :CONTINUE_OUTCOME, :REPAIR_PENDING_OUTCOME,
                        :TERMINAL_OUTCOMES, :COMPACT_ACCEPTED, :FAST_PATH_HOOK_NAMES, :FAST_PATH_HOOK_REFERENCES,
-                       :FastPathMutationTracker, :FastPathClassMutationTracker,
-                       :EMPTY_GREEN_TOKEN_STATES, :EMPTY_GREEN_MEMO_ENTRIES
+                       :FastPathMutationTracker, :FastPathClassMutationTracker
 
       # @rbs @yydebug: bool
       # @rbs @yydebug_output: IO
@@ -754,8 +749,8 @@ module Ibex
           preserve_existing && defined?(@runtime_fast_path_singleton_ancestors)
         @syntax_only = false unless preserve_existing && defined?(@syntax_only)
         @green_cache_override = nil unless preserve_existing && defined?(@green_cache_override)
-        @green_token_states = EMPTY_GREEN_TOKEN_STATES unless preserve_existing && defined?(@green_token_states)
-        @green_memo_stack = EMPTY_GREEN_MEMO_ENTRIES unless preserve_existing && defined?(@green_memo_stack)
+        @green_token_states = EMPTY_LOCATIONS unless preserve_existing && defined?(@green_token_states)
+        @green_memo_stack = EMPTY_LOCATIONS unless preserve_existing && defined?(@green_memo_stack)
         @green_parse_memo_valid = false unless preserve_existing && defined?(@green_parse_memo_valid)
         @green_initial_state = 0 unless preserve_existing && defined?(@green_initial_state)
         @syntax_parse_memo = nil unless preserve_existing && defined?(@syntax_parse_memo)
@@ -1831,7 +1826,7 @@ module Ibex
       end
 
       # @rbs (Hash[Symbol, untyped] tables) -> void
-      def prepare_green_cst(tables)
+      def prepare_green_cst(tables) # rubocop:disable Metrics/MethodLength
         @syntax_root = nil
         @syntax_parse_memo = nil
         config = tables[:cst]
@@ -1841,12 +1836,17 @@ module Ibex
           @green_cache = nil
           @syntax_diagnostics = EMPTY_LOCATIONS
           @green_pending_skipped = EMPTY_GREEN_TRIVIA
-          @green_token_states = EMPTY_GREEN_TOKEN_STATES
-          @green_memo_stack = EMPTY_GREEN_MEMO_ENTRIES
+          @green_token_states = EMPTY_LOCATIONS
+          @green_memo_stack = EMPTY_LOCATIONS
           return
         end
 
-        prepare_active_green_cst_buffers
+        @syntax_diagnostics = [] if @syntax_diagnostics.frozen?
+        @green_pending_skipped = [] if @green_pending_skipped.frozen?
+        @green_token_states = [] if @green_token_states.frozen?
+        @green_memo_stack = [] if @green_memo_stack.frozen?
+        @syntax_diagnostics.clear
+        @green_pending_skipped.clear
         kinds = tables[:cst_kinds]
         kinds = CST::Kind.new(config.fetch(:kinds), slots: config.fetch(:slots)) unless kinds.is_a?(CST::Kind)
         cache = @green_cache_override || CST::NodeCache.new
@@ -1859,16 +1859,6 @@ module Ibex
         @green_reused_right_edge = false
         @incremental_reused_descendants = 0
         @green_builder = CST::GreenBuilder.new(kinds: kinds, cache: cache)
-      end
-
-      # @rbs () -> void
-      def prepare_active_green_cst_buffers
-        @syntax_diagnostics = [] if @syntax_diagnostics.equal?(EMPTY_LOCATIONS)
-        @green_pending_skipped = [] if @green_pending_skipped.equal?(EMPTY_GREEN_TRIVIA)
-        @green_token_states = [] if @green_token_states.equal?(EMPTY_GREEN_TOKEN_STATES)
-        @green_memo_stack = [] if @green_memo_stack.equal?(EMPTY_GREEN_MEMO_ENTRIES)
-        @syntax_diagnostics.clear
-        @green_pending_skipped.clear
       end
 
       # @rbs (Hash[Symbol, untyped] tables) -> void
