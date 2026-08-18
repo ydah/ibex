f16ac14c0a31b5b09f4f234ea901abc74c118593
ydah <t.yudai92@gmail.com>
2026-08-19 08:05:09 +0900

    Define autoload namespaces before dependencies

--- lib/ibex/verify.rb
+++ lib/ibex/verify.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 # rbs_inline: enabled
 
+module Ibex
+  module Verify
+  end
+end
+
 require_relative "analysis"
 require_relative "tables"
 require_relative "verify/reference_collection"
@@ -8,8 +13,3 @@ require_relative "verify/result"
 require_relative "verify/verifier"
 require_relative "verify/action_correspondence"
 require_relative "verify/language_witness"
-
-module Ibex
-  module Verify
-  end
-end
